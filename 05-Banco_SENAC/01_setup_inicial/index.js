baece452ccf04d21e60ac44c2c3ac45bba0ab715
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-11-04 19:46:13 -0300

    01_setup_inicial

---
+++ 05-Banco_SENAC/01_setup_inicial/index.js
@@ -0,0 +1,8 @@
+// modulos Externos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+//modulos interno
+const fs = require('fs')
+
+console.log(chalk.bgGreen("Bem vindo ao Banco Senac!"));
