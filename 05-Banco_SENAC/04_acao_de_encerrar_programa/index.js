797f9870d8682341ae15b1ab884ed4c507d54c52
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-11-05 19:52:55 -0300

    ~depositar 2

--- 05-Banco_SENAC/04_acao_de_encerrar_programa/index.js
+++ 05-Banco_SENAC/04_acao_de_encerrar_programa/index.js
@@ -156,7 +156,30 @@ function deposit() {
     })
 }
 
-function addAmount(accountName, amount) { }
+function addAmount(accountName, amount) {
+  const accountData = getAccount(accountName)
+
+  if (!amount) {
+    console.log(
+      chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'),
+    )
+    return deposit()
+  }
+
+  accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+  fs.writeFileSync(
+    `accounts/${accountName}.json`,
+    JSON.stringify(accountData),
+    function (err) {
+      console.log(err)
+    },
+  )
+
+  console.log(
+    chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`),
+  )
+}
 
 function withdraw() { }
 
