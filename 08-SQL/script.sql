ba5ceeb14b88db81b9ee66c41565b6631c22dd77
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-11-08 19:49:32 -0300

    aula 08-11

---
+++ 08-SQL/script.sql
@@ -0,0 +1,13 @@
+-- Mostra as bases de dados da máquina
+show databases;
+
+-- Criar um banco de dados
+create database dbloja;
+
+-- acessando o banco de dado
+use dbloja;
+
+-- visualizando as tabelas
+show tables; 
+
+-- Criar tabela
