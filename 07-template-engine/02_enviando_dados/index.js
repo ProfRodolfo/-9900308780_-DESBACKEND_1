f493fa235dadd5a46d91dff66d923b80b1a7ec99
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-11-07 21:38:56 -0300

    aula

--- 07-template-engine/02_enviando_dados/index.js
+++ 07-template-engine/02_enviando_dados/index.js
@@ -1,23 +1,20 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const app = express();
+const express = require('express')
+const exphbs = require('express-handlebars')
 
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
+const app = express()
+app.engine('handlebars', exphbs.engine())
+//app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
-app.get("/", (req, res) => {
-    
-    const user = {
-        name: "João",
-        surname: "Silva",
-        age: 20,
-        
-    }
-    
-    
-    res.render("home", {user: user});
-});
+app.get('/', function (req, res) {
+  const user = {
+    name: 'Rodolfo',
+    surname: 'Gonçalves',
+    age: 38,
+  }
+  const work = "Programador"
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+  res.render('home', { user: user , work })
+})
+
+app.listen(3000)
