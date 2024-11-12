b11ab8edd0c0b6a0bee70935df86e917ad8629e7
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-11-11 22:06:24 -0300

    finalizado

--- 09-proj_crud/server/routes/user.js
+++ 09-proj_crud/server/routes/user.js
@@ -4,5 +4,12 @@ const router = express.Router();
 const userController = require('../controllers/userController.js');
 
 router.get('/', userController.view);
-
+router.post('/', userController.find);
+router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
+router.get('/edituser/:id', userController.edit);
+router.post('/edituser/:id', userController.update);
+router.get('/viewuser/:id', userController.viewall);
+router.get('/:id',userController.delete);
+  
 module.exports = router;
